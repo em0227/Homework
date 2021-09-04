a68b8069aa9d5029716396eb1d5df9c3413c8d79
Emily Wu <emilywu0227@gmail.com>
2021-09-04 17:58:16 -0400

    finished rspec hw

--- W4D4/Rspec/spec/dessert_spec.rb
+++ W4D4/Rspec/spec/dessert_spec.rb
@@ -6,8 +6,8 @@ Instructions: implement all of the pending specs (the `it` statements without bl
 =end
 
 describe Dessert do
-  let(:chef) { double("chef") }
-  let(:test) { Dessert.new('brownie', 50, 'Emily')}
+  let(:chef) { double("chef", name: "Emily")}#, :titleize => "Chef Emily the Great Baker")} this can works  #1. try to put the method here but won't work 2. didn't put the name method, put the name in the double's first '' originally but still works like that
+  let(:test) { Dessert.new('brownie', 50, chef)} #put 'Emily' as chef at first, changed then it work for serve method.
 
   describe "#initialize" do
     
@@ -29,28 +29,53 @@ describe Dessert do
   end
 
   describe "#add_ingredient" do
-
     it "adds an ingredient to the ingredients array" do
-      expect(test.ingredients).to eq(['chocolate'])
       test.add_ingredient('chocolate')
+      expect(test.ingredients).to eq(['chocolate']) #can also use to include
     end
   end
 
   describe "#mix!" do
-    it "shuffles the ingredient array"
+    it "shuffles the ingredient array" do
+      original_ingre = ['chocolate', 'milk', 'flour']
+      test.add_ingredient('chocolate')
+      test.add_ingredient('milk')
+      test.add_ingredient('flour')
+      #original_ingre = test.ingredients #doesn't work due to psr
+      expect(test.mix!).to_not eq(original_ingre) 
+    end
   end
 
   describe "#eat" do
-    it "subtracts an amount from the quantity"
+    it "subtracts an amount from the quantity" do
+      test.eat(20)
+      expect(test.quantity).to eq(30)
+    end
 
-    it "raises an error if the amount is greater than the quantity"
+    it "raises an error if the amount is greater than the quantity" do 
+      #test.eat(60)
+      expect {test.eat(60)}.to raise_exception #or can do to raise_error("not enough left!")   
+    end
+
   end
 
   describe "#serve" do
-    it "contains the titleized version of the chef's name"
+    it "contains the titleized version of the chef's name" do
+     allow(chef).to receive(:titleize).and_return("Chef Emily the Great Baker") #no matter here or in double, can't use #{name}
+      expect(test.serve).to eq("Chef Emily the Great Baker has made 50 brownies!")
+    end
+
   end
 
   describe "#make_more" do
-    it "calls bake on the dessert's chef with the dessert passed in"
+    it "calls bake on the dessert's chef with the dessert passed in" do
+      # allow(chef).to receive(:bake(test)).and_return {
+      #   test.mix!
+      #   test.heat!}
+      allow(chef).to receive(:bake).with(test)
+      test.make_more
+      #expect(test.mix!)
+      #expect(test.temp).to eq(400)
+    end
   end
 end
